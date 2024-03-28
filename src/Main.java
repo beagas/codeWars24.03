346bfe377c6b1cb2de490d27f78884ec8824bcc9
beagas <beatrice.gaskiene@gmail.com>
2024-03-28 22:33:04 +0200

    How long should you cook this for?7kyu. 2 cases doesn't work

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,11 @@ public class Main {
         Solution.replace("KATINUKAS");
         System.out.println((Solution.removeExclamationMarks("Hello!")));
         System.out.println(Solution.collinearity(1,1,6,1));
+        System.out.println(Solution.cockroachSpeed(89));
+        System.out.println(Solution.paperWork(5,2));                                         //"83W", 61, 80, "26W"
+        System.out.println(Solution.getTime("38W", 95, 22, "12W"));//"450W", 3, 25, "950W"
+        System.out.println(63 / 60);
+        System.out.println(63 % 60);
     }
 }
 
--- src/Solution.java
+++ src/Solution.java
@@ -22,4 +22,29 @@ public class Solution {
             return false;
         }
     }
+
+        public static int cockroachSpeed(double x){
+            int speed = (int) Math.floor((x*1000)/36);
+            return speed;
+        }
+
+    public static int paperWork(int n, int m) {
+        int allPapers = 0;
+        if (n > 0 && m > 0) {
+            allPapers = n * m;
+        }
+        return allPapers;
+    }
+    public static String getTime(String neededPower, int minutes, int seconds, String power)
+    {
+        int initialSeconds = minutes * 60 + seconds;
+        int neededPowerValue = Integer.parseInt(neededPower.substring(0, neededPower.length() - 1));
+        int powerValue = Integer.parseInt(power.substring(0, power.length() - 1));
+        long neededTimeSeconds = (Math.round((double)neededPowerValue * initialSeconds / powerValue));
+        int newMinutes = (int)((neededTimeSeconds) / 60);
+        int newSeconds = (int)((neededTimeSeconds) % 60);
+        String answer = newMinutes + " minutes " + newSeconds + " seconds";
+        return answer;
+
+    }
 }
