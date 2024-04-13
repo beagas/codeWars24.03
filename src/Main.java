451b64ce897032d417a92630933326afca559ed6
beagas <beatrice.gaskiene@gmail.com>
2024-04-13 18:36:13 +0300

    <7kyu> How long should you cook this for? (2 versions)

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         System.out.println((Solution.removeExclamationMarks("Hello!")));
         System.out.println(Solution.collinearity(1,1,6,1));
         System.out.println(Solution.cockroachSpeed(89));
-        System.out.println(Solution.paperWork(5,2));                                         //"83W", 61, 80, "26W"
+        System.out.println(Solution.paperWork(5,2));
         System.out.println(Solution.getTime("83W",61,80,"26W"));//"450W", 3, 25, "950W"
         System.out.println(Solution.getTime1("83W",61,80,"26W"));
         System.out.println(Solution.hexColor(""));
